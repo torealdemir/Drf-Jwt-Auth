72a146a0ddf3b5fc377ea215dc8997f5b1e85819
torealdemir <aldemirtore@gmail.com>
2023-12-14 14:50:58 +0300

    fixed settings file error

--- server/core/settings.py
+++ server/core/settings.py
@@ -125,8 +125,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-REST_FRAMEWORK = [
+REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework_simplejwt.authentication.JWTAuthentication'
-    ]
-]
+    ],
+}
